f0e012a9e10b86eee06fb4c359f547f3ffbb22bb
Hacker Jr <70173542+HackerX7@users.noreply.github.com>
2021-12-27 15:00:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -37,15 +37,15 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ 🧨', url='http://t.me/Maakri_filter_bot?startgroup=true')
+            InlineKeyboardButton('👑 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ 👑', url='http://t.me/Maakri_filter_bot?startgroup=true')
             ],[
-            InlineKeyboardButton('Sᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('Discussion 🎭', url='https://t.me/Cinephile_reviews')
+            InlineKeyboardButton('🌝 Sᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('📝 Discussion', url='https://t.me/Cinephile_reviews')
             ],[
-            InlineKeyboardButton('Gʀᴏᴜᴘ & Cʜᴀɴɴᴇʟ Lɪɴᴋs', callback_data='source')
+            InlineKeyboardButton('✨ Gʀᴏᴜᴘ & Cʜᴀɴɴᴇʟ Lɪɴᴋs ✨', callback_data='source')
             ],[
-            InlineKeyboardButton('Hᴇʟᴘ', callback_data='help'),
-            InlineKeyboardButton('Aʙᴏᴜᴛ', callback_data='about')
+            InlineKeyboardButton('🧑‍🦯 Hᴇʟᴘ', callback_data='help'),
+            InlineKeyboardButton('😎 Aʙᴏᴜᴛ', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_video(
